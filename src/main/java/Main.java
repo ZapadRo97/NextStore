92ee0ed8417ade8cade5c21ed695ea44562d9cf3
Zapad <zapad.daniel7@gmail.com>
2018-05-01 13:16:53 +0300

    mustache working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,9 @@
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
@@ -7,11 +12,17 @@ public class Main {
         //TODO: create folders for Model, View, Controller
         //get("/hello", (req, res) -> "Hello World");
 
-        Controller controller = new Controller();
+        //Controller controller = new Controller();
         //controller.addUser("Matei", "Ioan", "matio", "mation@mat.io", "12345", "0734");
         //controller.deleteUser(3);
         //ArrayList<User> au = controller.getUsersList();
         //System.out.println("Number of users: " + au.size());
         //System.out.println(au.toString());
+
+        Map map = new HashMap();
+        map.put("name", "Sam");
+
+        // hello.mustache file is in resources/templates directory
+        get("/hello", (rq, rs) -> new ModelAndView(map, "hello.mustache"), new MustacheTemplateEngine());
     }
 }
